f74f1c352d02a607cfdf262a2a5efb7c44099442
Salah Al Shayah <122882654+salahalshayah@users.noreply.github.com>
2023-04-06 00:15:44 +0300

    Update CharacterFrequencyCounter.java
    - Removed import for BufferUnderflowException.
    - Implemented the functionality to filter results based on a minimum frequency threshold.
    - Expanded the range of supported characters from 500 to 5000.
    - Replaced ASCII codes with Unicode code points.
    - Corrected the issue of incorrect results format with Space, Tab, and New Line characters.
    - Properly closed all Scanners.

--- CharacterFrequencyCounter.java
+++ CharacterFrequencyCounter.java
@@ -4,20 +4,28 @@ import java.io.IOException;
 import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.io.PrintWriter;
-import java.nio.BufferUnderflowException;
+
 public class CharacterFrequencyCounter{
     public static void main(String[] args) throws IOException{
-        Scanner sc =new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.print("Please enter the name of the file: ");
         String inputFileName = sc.nextLine();
-        System.out.println();
+
+        System.out.print("Please enter the minimum time a character should appear to be counted seperatly: ");
+        int countMin = sc.nextInt();
+
         File file = new File(inputFileName);
         Scanner inputScanner = new Scanner(file);
         inputScanner.useDelimiter("");
-        int[] charFrequencyArray = new int[500];
+
+        int[] charFrequencyArray = new int[5000];
+        char[] mostFrequentChar = new char[5000];
+        int[] mostFrequentCharCount = new int[5000];
         int otherCount = 0;
-        char[] mostFrequentChar = new char[500];
-        int[] mostFrequentCharCount = new int[500];
+        int maxCount;
+        int idxmax;
 
         FileWriter fileWrite = new FileWriter("results.txt");
         BufferedWriter bufferedWriter = new BufferedWriter(fileWrite);
@@ -25,42 +33,59 @@ public class CharacterFrequencyCounter{
 
         while(inputScanner.hasNext()){
             String thistoken = inputScanner.next();
-            if((thistoken.charAt(0)) < 500){
+            if((thistoken.charAt(0)) < charFrequencyArray.length){
             char thischar = thistoken.charAt(0);
             charFrequencyArray[0 + thischar]++;
             }
             else otherCount++;
         }
         
-        int maxCount = 0;
-        int idxmax = 0;
+
         for(int idd = 0; idd<charFrequencyArray.length; idd++){
             maxCount = 0;
             idxmax = 0;
+
             for(int i = 0; i < charFrequencyArray.length; i++){
+
                 if (charFrequencyArray[i] > maxCount) {
                     maxCount = charFrequencyArray[i];
                     idxmax = i;
                     charFrequencyArray[i] = 0;
                 }
+
                 mostFrequentChar[idd] = ((char) idxmax);
                 mostFrequentCharCount[idd] = maxCount;
             }
         }
         
         printWriter.println("Character Frequencies for File: " + inputFileName);
-        printWriter.println("===============================================");
-        printWriter.println("Character\t| ASCII Code\t| Frequency");
-        printWriter.println("-----------------------------------------------");
+        printWriter.println("=====================================================");
+        printWriter.println("Character\t| Unicode code point\t| Frequency");
+        printWriter.println("-----------------------------------------------------");
         for (int i = 0; i < charFrequencyArray.length; i++) {
             if (mostFrequentCharCount[i] == 0) {
                 continue;
-            } else {
-                printWriter.printf("%c\t\t| %d\t\t| %d\n", mostFrequentChar[i], (int) mostFrequentChar[i], mostFrequentCharCount[i]);
+            }else{
+
+                if(mostFrequentCharCount[i]<countMin){
+                    otherCount += mostFrequentCharCount[i];
+                }else{
+                    if(mostFrequentChar[i] == '\s'){
+                        printWriter.printf("%s\t\t|\tU+%04X\t\t|%d", "Space", (int) mostFrequentChar[i], mostFrequentCharCount[i]);
+                    }else if(mostFrequentChar[i] == '\t'){
+                        printWriter.printf("%s\t\t|\tU+%04X\t\t|%d", "Tab", (int) mostFrequentChar[i], mostFrequentCharCount[i]);
+                    }else if(mostFrequentChar[i] == '\n'){
+                        printWriter.printf("%s\t|\tU+%04X\\tt|%d", "New Line", (int) mostFrequentChar[i], mostFrequentCharCount[i]);
+                    }else{
+                        printWriter.printf("%c\t\t|\tU+%04X\t\t|%d\n", mostFrequentChar[i], (int) mostFrequentChar[i], mostFrequentCharCount[i]);                
+                    }
+                }
             }
         }
-        printWriter.println("-----------------------------------------------");
+        printWriter.println("-----------------------------------------------------");
         printWriter.printf("Other Characters\t| %d\n", otherCount);
         printWriter.close();
+        sc.close();
+        inputScanner.close();
     }
 }
